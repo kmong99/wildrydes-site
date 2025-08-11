d200649c87fea1139d290bbd6c062419636c0507
kmong99 <131394844+kmong99@users.noreply.github.com>
2025-08-10 19:19:29 -0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,11 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-west-2_OSk9hdStq', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '4mi8aoir34lamsi98j2b92bv1r', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-west-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
